fd462b50e952d9c1186e39b7deffe98b673b220b
AnushaKhan <theg8anusha72@gmail.com>
2024-09-02 22:47:13 +0530

    starting new proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,23 @@
-import React from "react";
-import {
-  wrapperDiv,
-  balanceWrapper,
-  moneyWrapper,
-  expincWrapper,
-  inccontainer,
-  expincHead,
-  mainInc,
-  mainExp,
-  divider,
-  expcontainer,
-  historyWrapper,
-  addTransaction,
-  transWrapper,
-  descWrapper,
-  radioWrapper,
-  amtWrapper,
-  incWrapper,
-  expWrapper,
-  h1Wrapper,
-  h2Wrapper
-} from "./styles";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Dashboard from "./dashboard";
 
-const App = () => {
+function App() {
   return (
-    <div style={wrapperDiv}>
-      <h1 style={h1Wrapper}>expense tracker</h1>
-      <div style={balanceWrapper}>
-        <h2 style={h2Wrapper}>Your balance</h2>
-        <p>
-          <span>$</span>
-          <span style={moneyWrapper}> 0.0</span>
-        </p>
-      </div>
-      <div style={expincWrapper}>
-        <div style={inccontainer}>
-          <h2 style={expincHead}>Income</h2>
-          <p>
-            <span>₹</span>
-            <span style={mainInc}>0.0</span>
-          </p>
-        </div>
-        <div style={divider}></div>
-        <div style={expcontainer}>
-          <h2 style={expincHead}>Expense</h2>
-          <p>
-            <span>₹</span>
-            <span style={mainExp}>0.0</span>
-          </p>
+    <BrowserRouter>
+      <div className="main-wrapper">
+        <div className="sidebar"></div>
+        <div>
+          <header> </header>
+          <div>
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/" element={<>Hello</>} />
+            </Routes>
+          </div>
         </div>
       </div>
-
-      <div style={historyWrapper}>
-        <h2 style={h2Wrapper}>History</h2>
-      </div>
-      <div style={addTransaction}>
-        <h2 style={h2Wrapper}>Add Transaction</h2>
-      </div>
-      <form style={transWrapper}>
-        <label htmlFor="desc">Description</label>
-        <input type="text" name="desc" style={descWrapper} required />
-        <label htmlFor="amt">Amount</label>
-        <input type="text" name="amt" style={amtWrapper} required />
-        <div style={radioWrapper}>
-          <input type="radio" name="abc" style={incWrapper} checked />
-          <label htmlFor="inc">Income</label>
-          <input type="radio" name="abc" style={expWrapper} checked />
-          <label htmlFor="exp">Expense</label>
-        </div>
-        <button type="submit"> Submit</button>
-      </form>
-    </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// const Comp = () => {
+//   const [state, setState] = useState(0);
+//   useEffect(() => {
+//     const t = setInterval(() => {
+//       console.log(state);
+//       setState((prev) => prev + 1);
+//     }, 1000);
+//     return () => {
+//       clearInterval(t);
+//     };
+//   }, []);
+//   return <App />;
+// };
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.body!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
